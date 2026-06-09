615f10e52d89251320e954fae9f15cdd5ecfa31b
Toan <domanht7@gmail.com>
2026-06-10 02:15:46 +0700

    add @Builder.Default

---
+++ com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/service/PatientHealthService.java
@@ -0,0 +1,28 @@
+package com.rpm.remotepatientmonitoring.service;
+
+import com.rpm.remotepatientmonitoring.dto.HealthLogRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.stereotype.Service;
+import java.util.Map;
+@Service
+public class PatientHealthService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    public Map<String, Object> submitDailyHealthLog(HealthLogRequest req) {
+        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("sp_record_daily_health_log");
+        MapSqlParameterSource inParams = new MapSqlParameterSource();
+        inParams.addValue("patient_id", req.getPatientId());
+        inParams.addValue("log_type", req.getLogType());
+        inParams.addValue("input_method", req.getInputMethod());
+        inParams.addValue("systolic_bp", req.getSystolicBp());
+        inParams.addValue("diastolic_bp", req.getDiastolicBp());
+        inParams.addValue("heart_rate", req.getHeartRate());
+        inParams.addValue("glucose_level", req.getGlucoseLevel());
+        inParams.addValue("image_url", req.getImageUrl());
+        inParams.addValue("patient_notes", req.getPatientNotes());
+        return jdbcCall.execute(inParams);
+    }
+}
