4523dcb0df3a7296f6afb1ac36e17f1dc0370d19
Toan <domanht7@gmail.com>
2026-06-10 02:31:56 +0700

    Building a Patient Dashboard

---
+++ com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/config/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.rpm.remotepatientmonitoring.config;
+
+import com.rpm.remotepatientmonitoring.model.Account;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+    private final Account account;
+
+    public CustomUserDetails(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + account.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return account.getIsActive();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return account.getIsActive();
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
---
+++ com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/repository/PatientRepository.java
@@ -0,0 +1,16 @@
+package com.rpm.remotepatientmonitoring.repository;
+
+import com.rpm.remotepatientmonitoring.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
+    Optional<Patient> findByPatientCode(String patientCode);
+    Optional<Patient> findByAccountId(Integer accountId);
+    Optional<Patient> findByPhone(String phone);
+    List<Patient> findByDoctorId(Integer doctorId);
+    List<Patient> findByHospitalId(Integer hospitalId);
+}
