5fbfb2ec49ad7d89b1f37874925ef9e1abe5b0dd
Toan <domanht7@gmail.com>
2026-06-10 02:52:05 +0700

    Reapply "Building a Patient Dashboard"
    This reverts commit f706ceb35dc81ad753258da69043be07cdeef0b8.

---
+++ com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/repository/HealthLogRepository.java
@@ -0,0 +1,12 @@
+package com.rpm.remotepatientmonitoring.repository;
+
+import com.rpm.remotepatientmonitoring.model.DailyHealthLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface HealthLogRepository extends JpaRepository<DailyHealthLog, Integer> {
+    List<DailyHealthLog> findByPatientIdAndLogDate(Integer patientId, LocalDate logDate);
+}
