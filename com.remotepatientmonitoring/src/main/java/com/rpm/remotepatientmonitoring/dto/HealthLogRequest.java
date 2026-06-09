62c8e3f001726d036f29f203081c45c41165031a
Toan <domanht7@gmail.com>
2026-06-10 02:15:46 +0700

    add @Builder.Default

---
+++ com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/dto/HealthLogRequest.java
@@ -0,0 +1,16 @@
+package com.rpm.remotepatientmonitoring.dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+@Data
+public class HealthLogRequest {
+    private Integer patientId;
+    private String logType;
+    private String inputMethod;
+    private Integer systolicBp;
+    private Integer diastolicBp;
+    private Integer heartRate;
+    private BigDecimal glucoseLevel;
+    private String imageUrl;
+    private String patientNotes;
+}
