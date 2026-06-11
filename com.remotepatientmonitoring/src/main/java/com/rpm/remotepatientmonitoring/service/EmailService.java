6615e72a598d4da9a77f27d19e8aa908e4d4706c
Duong Duc Thang <thang@example.com>
2026-06-11 17:09:57 +0700

    feat: Doctor auto-generate password and send email

---
+++ com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/service/EmailService.java
@@ -0,0 +1,35 @@
+package com.rpm.remotepatientmonitoring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public void sendDoctorPassword(String toEmail, String doctorName, String password) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject("Tài khoản bác sĩ - Hệ thống RPM");
+        message.setText(
+                "Xin chào Bác sĩ " + doctorName + ",\n\n" +
+                        "Tài khoản của bạn đã được tạo trên Hệ thống Theo dõi Bệnh nhân Từ xa (RPM).\n\n" +
+                        "Thông tin đăng nhập:\n" +
+                        "Email: " + toEmail + "\n" +
+                        "Mật khẩu: " + password + "\n\n" +
+                        "Vui lòng đổi mật khẩu ngay sau lần đăng nhập đầu tiên.\n\n" +
+                        "Trân trọng,\n" +
+                        "Hệ thống RPM"
+        );
+        mailSender.send(message);
+    }
+}
