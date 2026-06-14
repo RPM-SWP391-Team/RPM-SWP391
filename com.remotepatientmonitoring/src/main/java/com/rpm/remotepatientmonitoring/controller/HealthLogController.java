7628335ff6cb90e8d5ef736499f8925f82cd4e0e
Toan <domanht7@gmail.com>
2026-06-15 05:51:34 +0700

    fix enter

--- com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/controller/HealthLogController.java
+++ com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/controller/HealthLogController.java
@@ -27,6 +27,7 @@ public class HealthLogController {
 
     @GetMapping("/log")
     public String showLogForm(Model model) {
+        Patient patient = patientRepository.findAll().stream().findFirst().orElse(null);
         DailyHealthLog log = DailyHealthLog.builder()
                 .logDate(LocalDate.now())
                 .logTime(LocalDateTime.now())
@@ -35,6 +36,7 @@ public class HealthLogController {
                 .isAlertProcessed(false)
                 .build();
         model.addAttribute("healthLog", log);
+        model.addAttribute("patient", patient);
         return "patient/log";
     }
 
