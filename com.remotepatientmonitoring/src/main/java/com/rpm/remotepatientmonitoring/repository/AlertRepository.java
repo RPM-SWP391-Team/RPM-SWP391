9b4dbd4a4cb63ed16fd40ab4c5251a25861a54f8
chuoizz <25092005thang@gmail.com>
2026-06-19 09:09:10 +0700

    Update code

---
+++ com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/repository/AlertRepository.java
@@ -0,0 +1,13 @@
+package com.rpm.remotepatientmonitoring.repository;
+
+import com.rpm.remotepatientmonitoring.model.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlertRepository extends JpaRepository<Alert, Integer> {
+    @Query("SELECT COUNT(a) FROM Alert a WHERE a.doctor.id = :doctorId AND UPPER(a.alertColor) = UPPER(:alertColor) AND a.isResolved = false")
+    long countUnresolvedAlertsByColor(@Param("doctorId") Integer doctorId, @Param("alertColor") String alertColor);
+}
