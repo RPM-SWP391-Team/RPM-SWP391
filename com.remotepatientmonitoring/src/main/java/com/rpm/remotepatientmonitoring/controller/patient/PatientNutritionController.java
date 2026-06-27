6e594281462f6c18a1df1f73f53c52bb7291de05
Toan <domanht7@gmail.com>
2026-06-27 16:40:06 +0700

    Resolve DDL validation error for CHK_diet_logs_quantity constraint by using BigDecimal type for quantityG in PatientMeal

--- com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/controller/patient/PatientNutritionController.java
+++ com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/controller/patient/PatientNutritionController.java
@@ -74,7 +74,7 @@ public class PatientNutritionController {
         meal.setLogDate(today);
         meal.setMealType(mealType);
         meal.setFood(food);
-        meal.setQuantityG(quantityG);
+        meal.setQuantityG(java.math.BigDecimal.valueOf(quantityG));
 
         PatientMeal saved = patientMealRepository.save(meal);
 
