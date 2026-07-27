3e7ef6ee810ee40281b08ea8c36433c92f7ce2cf
Deku-coder <lethang162005@gmail.com>
2026-07-27 15:47:17 +0700

    Fix giao dien tren feat/admin_fix_ver2

--- com.remotepatientmonitoring/src/test/java/com/rpm/remotepatientmonitoring/service/HospitalConfigServiceTest2.java
+++ com.remotepatientmonitoring/src/test/java/com/rpm/remotepatientmonitoring/service/HospitalConfigServiceTest2.java
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class HospitalConfigServiceTest {
+public class HospitalConfigServiceTest2 {
 
     @Mock
     private AuditTrailRepository auditTrailRepository;
