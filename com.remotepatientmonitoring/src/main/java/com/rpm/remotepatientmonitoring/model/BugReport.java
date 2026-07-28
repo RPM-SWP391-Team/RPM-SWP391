a35025e7329856f60942a752ee65c2d6db581fe2
chuoizz <25092005thang@gmail.com>
2026-07-28 08:43:44 +0700

    fix(model): revert @Nationalized annotation from BugReport title to fix SQLServerException unsupported conversion from varchar to NCHAR error

--- com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/model/BugReport.java
+++ com.remotepatientmonitoring/src/main/java/com/rpm/remotepatientmonitoring/model/BugReport.java
@@ -22,8 +22,7 @@ public class BugReport {
     @JoinColumn(name = "account_id", nullable = false)
     private Account account;
 
-    @org.hibernate.annotations.Nationalized
-    @Column(nullable = false, length = 255, columnDefinition = "NVARCHAR(255)")
+    @Column(nullable = false, length = 255)
     private String title;
 
     @Column(nullable = false, columnDefinition = "NVARCHAR(MAX)")
